43987ecd9d5a20a4706e6dea5bf69a29702426fa
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-04-20 17:49:06 +0530

    addding the abnonymous function logic

--- script.js
+++ script.js
@@ -13,56 +13,64 @@
 // let and const is considered as block scoped..
 // var is considered as global scope 
 let a = 32;
-if(5>4){
+if (5 > 4) {
     {
-    let a = 34;
-    console.log(a);
+        let a = 34;
+        console.log(a);
     }
-console.log(a);
+    console.log(a);
 }
 
 // Anonymous function
 // here we are storing the function in a variable
 // Syntax: var variablename = function(){}
-var mul = function(a,b){
-return a*b;
+var mul = function (a, b) {
+    return a * b;
 }
-console.log(mul(12,13));
+console.log(mul(12, 13));
 
-function foo(){
+function foo() {
     var res = 30;
     return "hello world";
-    }
-    console.log(foo());
-    
-    // Arrow Functions
-    // They are introduced in ES6
-    // Syntax: var variablename = ()=>{}
-    // 2 different usecase
-    // 1st use case..
-    // Suppose if the Function is returning a single value
-    var res = ()=>"hello world";
-    console.log(res());
-    
-    var data = (a,b)=> a*b;
-    console.log(data(12,13));
-    
-    // if a function is returning a single value
-    //then there is no need of return keyword in a arrow functions
-    
-    
-    // 2nd Use case
-    // if a function is having the Multiple lines 
-    //then there is the need of return keyword...
-    var data_res = ()=>{
+}
+console.log(foo());
+
+// Arrow Functions
+// They are introduced in ES6
+// Syntax: var variablename = ()=>{}
+// 2 different usecase
+// 1st use case..
+// Suppose if the Function is returning a single value
+var res = () => "hello world";
+console.log(res());
+
+var data = (a, b) => a * b;
+console.log(data(12, 13));
+
+// if a function is returning a single value
+//then there is no need of return keyword in a arrow functions
+
+
+// 2nd Use case
+// if a function is having the Multiple lines 
+//then there is the need of return keyword...
+var data_res = () => {
     var res = 30;
     return "hello world";
+}
+
+// Write a anonymous function to print the Odd numbers froim the Given array
+// Input:[12,13,14,15,16,17,18];
+// Output:[13,15,17];
+
+var res = function (arr) {
+    var temp = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 != 0) {
+            temp.push(arr[i]);
+        }
     }
-    
-    // Write a anonymous function to print the Odd numbers froim the Given array
-    // Input:[12,13,14,15,16,17,18];
-    // Output:[13,15,17];
-    
-    
-    
-    
+    return temp;
+}
+console.log(res([12, 13, 14, 15, 16, 17, 18]));
+
