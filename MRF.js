e819df3db5a0d096a9cba739ab7ea0e4541f209f
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-04-27 16:34:01 +0530

    Update MRF.js

--- MRF.js
+++ MRF.js
@@ -18,3 +18,26 @@ var arr = [12,13,14,15,16,17];
 // SyntaxL: arrayname.map(functionname);
 var res = arr.map((ele)=>ele*2);
 console.log(res);
+
+
+
+// MRF Method and Examples...
+// Map,Reduce,filter
+var arr = [12,13,14,15,16,17];
+// Result:
+// var res = [12,14,16];
+// Step 01: Apply for loop
+// Step 02:take each element from the array and check whether it is even Number
+// Step 03: If it is a even number, We will be pusing it into temproray array
+// Step 04: If it is a Odd Number we will be skipping the array elements
+// Step 05: Print the resultant array
+
+
+ // Filter:
+ // It is applicable to arrays
+ // It will take the function as the parameter
+ // Inside the function we have the condition
+ // It will apply on each and every element of the array
+ // If the condition is successfull it will be in resultant array
+ var res =  arr.filter((ele)=>ele%2==0);
+ console.log(res);
